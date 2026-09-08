e5a9ac95c75aaa5cf69be285a92166c5d4a1e721
Emily Ussery <eussery122@mycr.redwoods.edu>
2026-09-08 16:17:18 -0700

    intitely breaking the code

--- src/main/java/dungeonforge/Main.java
+++ src/main/java/dungeonforge/Main.java
@@ -16,7 +16,7 @@ public final class Main {
     /** Bumped every sprint. Week 3 replaces this with the GameConfig singleton. */
     public static final String VERSION = "0.1.0";
 
-    private Main() { }
+    private Main()  {
 
     public static String banner() {
         return """
